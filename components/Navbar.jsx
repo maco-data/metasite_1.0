26ae146b894373d809ff5fb9ecb7c91513a80f5d
maco-data <mcospino@gmail.com>
2022-11-27 21:18:41 -0500

    Fix the styles import

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,7 @@
 
 import { motion } from "framer-motion"
 
-import { styles } from "../styles"
+import styles from "../styles"
 import { navVariants } from "../utils/motion"
 
 const Navbar = () => (
@@ -10,10 +10,10 @@ const Navbar = () => (
 		variants={navVariants}
 		initial="hidden"
 		whileInView="show"
-		className={`{styles.xPaddings} py-8 relative`}>
+		className={`${styles.xPaddings} py-8 relative`}>
 		<div className="absolute w-[50%] inset-0 gradient-01" />
 		<div
-			className={`{styles.innerWidth} mx-auto flex justify-between gap-8`}>
+			className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
 			<img
 				src="/search.svg"
 				alt="serach"
