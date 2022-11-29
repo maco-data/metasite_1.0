de0509837814942cd6c72dea447f5ce6b106a4f6
maco-data <mcospino@gmail.com>
2022-11-29 08:29:01 -0500

    Start explore section

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,9 +1,24 @@
-'use client';
+"use client"
+
+import { useState } from "react"
+import { motion } from "framer-motion"
+
+import styles from "../styles"
+import { exploreWorlds } from "../constants"
+import { staggerContainer } from "../utils/motion"
+import { ExploreCard, TitleText, TypingText } from "../components"
 
 const Explore = () => (
-  <section>
-    Explore section
-  </section>
-);
+	<section className={`${styles.paddings}`} id="explore">
+		<motion.div
+			variants={staggerContainer}
+			initial="hidden"
+			whileInView="show"
+			viewport={{ once: false, amount: 0.25 }}
+			className={`${styles.innerWidth} mx-auto flex flex-col`}>
+			<TypingText title="| The World" textStyles="text-center" />
+		</motion.div>
+	</section>
+)
 
-export default Explore;
+export default Explore
